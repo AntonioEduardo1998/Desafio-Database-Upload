f10ef103c862642da1385717ef1380acd78ac203
Antonio <antonioeduardo1998@gmail.com>
2020-06-29 22:04:25 -0300

    challenge completed

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,54 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    let findSameCategory = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!findSameCategory) {
+      findSameCategory = await categoryRepository.create({
+        title: category,
+      });
+    }
+
+    await categoryRepository.save(findSameCategory);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category: findSameCategory,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
